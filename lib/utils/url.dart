b784f8c87261d2baf74d9ceefb359f6e5e10662a
MrHumble21 <amalikov3@students.wiut.uz>
2023-03-24 23:32:13 +0500

    feat: url

--- lib/utils/url.dart
+++ lib/utils/url.dart
@@ -1,4 +1,5 @@
 import 'dart:io' show Platform;
 
 
-var platformUrl =  Platform.isAndroid ? "http://10.10.10.46:8000": "http://127.0.0.1:8000";
+// var platformUrl =  Platform.isAndroid ? "http://10.10.10.46:8000": "http://127.0.0.1:8000";
+var platformUrl =  'https://abdulboriymeritbackend.pythonanywhere.com';
